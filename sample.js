290c29833603a753b291642948372c4329093745
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-08-06 18:59:20 +0900

    平均点処理を追加

--- sample.js
+++ sample.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
+  let subject_points = [0, 0, 0, 0, 0];
+  let average;
   function score_indicate() {
     // このような記述をすることで、subject_pointsという変数の中に
     // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]という配列を作成できる。
-    let subject_points = [Number($('#national_language').val()),
-    Number($('#english').val()),
-    Number($('#mathematics').val()),
-    Number($('#science').val()),
-    Number($('#society').val())
+    subject_points = [
+      Number($('#national_language').val()),
+      Number($('#english').val()),
+      Number($('#mathematics').val()),
+      Number($('#science').val()),
+      Number($('#society').val())
     ];
 
     // さらにこのような記述をすることで、「合計点：」となっている右の部分に合計点が出力される
@@ -18,12 +21,14 @@ $(document).ready(function () {
     $("#sum_indicate").text(sum);
 
     // ここに、上記を参考にして平均点を出力する処理を書き込む
+    average = sum / subject_points.length;
+    $('#average_indicate').text(average);
   };
 
   function get_achievement() {
     // ここに、ランクの値の文字列（平均点が80点以上なら"A"、60点以上なら"B"、40点以上なら"C"、それ以下なら"D"）を出力する処理を書き込む
   }
-
+  
   function get_pass_or_failure() {
     // ここに、全ての教科が60点以上なら"合格"の文字列、一つでも60点未満の教科があったら"不合格"の文字列を出す処理を書き込む
   }
